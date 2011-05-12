7d1723821b90edd5283027f9bc690417d9b13f94
Alistair Stead <alistair.stead@designdisclosure.com>
2011-05-12 16:12:36 +0100

    created new roles

---
+++ roles/development.rb
@@ -0,0 +1,19 @@
+name "development"
+description "PHP Development system"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+run_list(
+  "php::codebrowser",
+  "php::codesniffer",
+  "php::copypaste",
+  "php::depend",
+  "php::documentor",
+  "php::dev",
+  "php::phpmd",
+  "php::phpunit",
+  "php::exdebug",
+  "magento::magetool"
+  )
+# Attributes applied if the node doesn't have it set already.
+default_attributes()
+# Attributes applied no matter what the node has set already.
+override_attributes()
---
+++ roles/webserver.rb
@@ -0,0 +1,15 @@
+name "webserver"
+description "PHP Webserver"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+run_list(
+  "apt",
+  "php",
+  "memcached", 
+  "apache2", 
+  "mysql::server",
+  "magento"
+  )
+# Attributes applied if the node doesn't have it set already.
+default_attributes()
+# Attributes applied no matter what the node has set already.
+override_attributes()
