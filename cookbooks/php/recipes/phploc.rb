2650cb515ed54604911c4154db662149076298e4
Ed van Beinum <ed.vanbeinum@sessiondigital.com>
2011-05-26 17:14:43 +0100

    Create Jenkins recipe, create phploc recipe,, Add Tomcat recipe, add ciserver role

---
+++ cookbooks/php/recipes/phploc.rb
@@ -0,0 +1,20 @@
+include_recipe "php::pear"
+
+channels = [ 
+    "components.ez.no"
+]
+
+channels.each do |chan|
+  php_pear_channel chan do
+    action :discover
+  end
+end
+
+pu = php_pear_channel "pear.phpunit.de" do
+  action :discover
+end
+
+php_pear "phploc" do
+  channel pu.channel_name
+  action :install
+end
