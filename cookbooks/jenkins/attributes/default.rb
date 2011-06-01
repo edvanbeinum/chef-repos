16070e821d05a8ebff0b4ba5fb09bf2b0be6bf64
Ed van Beinum <ed.vanbeinum@sessiondigital.com>
2011-06-01 11:33:17 +0100

    add missing Jenkins attribute file

---
+++ cookbooks/jenkins/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:jenkins][:apache][:servername] = "dreams.development.local"
+default[:jenkins][:apache][:server_alias] = Array.new
+default[:jenkins][:apache][:docroot] = "/var/lib/tomcat6/webapps/ROOT"
+
+default[:jenkins][:tomcat][:version] = "tomcat6"
