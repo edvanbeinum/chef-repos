cf89dd2fd122a80323eabbc56b28eb63f036eb54
Alistair Stead <alistair.stead@designdisclosure.com>
2011-06-14 22:18:30 +0100

    updated the ciserver role

--- roles/ciserver.rb
+++ roles/ciserver.rb
@@ -2,11 +2,10 @@ name "ciserver"
 description "Continuous Integration Webserver"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 run_list(
-	"tomcat",
 	"jenkins",
-	"jenkins::tomcat",
-	"jenkins::apache-tomcat",
-	"jenkins::php-jenkins-template"
+	"jenkins::apache-proxy",
+	"jenkins::php-jenkins-template",
+	"jenkins::github-deploy-key"
   )
 # Attributes applied if the node doesn't have it set already.
 default_attributes()
