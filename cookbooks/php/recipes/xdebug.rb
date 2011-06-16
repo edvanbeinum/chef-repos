364c7db4ad0e9e18161d5f0d3c1bacf35b769055
Alistair Stead <alistair.stead@designdisclosure.com>
2011-06-16 22:08:39 +0100

    refactored based on testing

--- cookbooks/php/recipes/xdebug.rb
+++ cookbooks/php/recipes/xdebug.rb
@@ -6,12 +6,18 @@ include_recipe "php::module_dev"
 #   action :install
 # end
 
-execute "install_xdebug" do
-  command "pecl install xdebug"
-  not_if "pecl list | grep xdebug"
+if platform?(%w{debian ubuntu})
+    execute "install_xdebug" do
+      command "pecl install xdebug"
+      not_if "pecl list | grep xdebug"
+    end
+
+    template "#{node['php']['ext_conf_dir']}/xdebug.ini" do
+      mode "0644"
+    end
+elsif platform?(%w{centos redhat fedora})
+    package "php53u-pecl-xdebug"
 end
 
-template "#{node['php']['ext_conf_dir']}/xdebug.ini" do
-  mode "0644"
-end
+
 
