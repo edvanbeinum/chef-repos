0c21e1ecdafa1d4120f905436d202b2bcb5241e2
Alistair Stead <alistair.stead@designdisclosure.com>
2011-06-30 16:13:01 +0100

    removed the use of the crontab cookbook and just created the functionality directly

--- cookbooks/magento/recipes/crontab.rb
+++ cookbooks/magento/recipes/crontab.rb
@@ -1,4 +1,14 @@
-crontab "magento-crontab" do 
-  username :root 
-  filename "magento-crontab.erb"
+crontabfile = "/tmp/chef-crontab.txt" 
+    
+template "crontab" do
+  path crontabfile
+  source "magento-crontab.erb"
+  mode 0655
+  variables(variables)
+end
+
+execute "crontab" do
+  command "crontab -u root #{crontabfile}"
+  action :run
+  timeout 60
 end
