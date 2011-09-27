23addeffcd90d5d4d9c784c38a5e8fc1c47140cf
Alistair Stead <alistair.stead@designdisclosure.com>
2011-09-27 09:04:45 +0100

    Added tuning values for Magento Created a recipe that will configure and start a slave

--- cookbooks/mysql/attributes/server.rb
+++ cookbooks/mysql/attributes/server.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-default['mysql']['bind_address']              = attribute?('cloud') ? cloud['local_ipv4'] : ipaddress
+default['mysql']['bind_address']              = ipaddress
 default['mysql']['datadir']                   = "/var/lib/mysql"
 
 case node["platform"]
@@ -45,27 +45,47 @@ default['mysql']['master'] = false
 default['mysql']['slave'] = false
 default['mysql']['relay_log'] = "mysql-relay-bin"
 default['mysql']['read_only'] = "1"
+default['mysql']['master_ip'] = ""
+default['mysql']['master_repl_password'] = ""
+default['mysql']['master_log_file'] = ""
 
 default['mysql']['tunable']['back_log']             = "128"
 default['mysql']['tunable']['key_buffer']           = "256M"
-default['mysql']['tunable']['max_allowed_packet']   = "16M"
+default['mysql']['tunable']['max_allowed_packet']   = "1M"
 default['mysql']['tunable']['max_connections']      = "800"
-default['mysql']['tunable']['max_heap_table_size']  = "32M"
+default['mysql']['tunable']['max_connect_errors']   = "10"
+default['mysql']['tunable']['max_heap_table_size']  = "1M"
 default['mysql']['tunable']['myisam_recover']       = "BACKUP"
 default['mysql']['tunable']['net_read_timeout']     = "30"
 default['mysql']['tunable']['net_write_timeout']    = "30"
 default['mysql']['tunable']['table_cache']          = "128"
 default['mysql']['tunable']['table_open_cache']     = "128"
 default['mysql']['tunable']['thread_cache']         = "128"
-default['mysql']['tunable']['thread_cache_size']    = 8
-default['mysql']['tunable']['thread_concurrency']   = 10
+default['mysql']['tunable']['thread_cache_size']    = "8"
+default['mysql']['tunable']['thread_concurrency']   = "10"
 default['mysql']['tunable']['thread_stack']         = "256K"
 default['mysql']['tunable']['wait_timeout']         = "180"
+default['mysql']['tunable']['sort_buffer_size']     = "1M"
+default['mysql']['tunable']['join_buffer_size']     = "1M"
+default['mysql']['tunable']['tmp_table_size']       = "1M"
+default['mysql']['tunable']['key_buffer_size']      = "1M"
+default['mysql']['tunable']['read_buffer_size']     = "1M"
+default['mysql']['tunable']['read_rnd_buffer_size'] = "1M"
+default['mysql']['tunable']['bulk_insert_buffer_size'] = "1M"
+
+
+default['mysql']['tunable']['myisam_sort_buffer_size'] = "1M"
+default['mysql']['tunable']['myisam_max_sort_file_size'] = "1M"
+default['mysql']['tunable']['myisam_repair_threads'] = "1"
 
 default['mysql']['tunable']['query_cache_limit']    = "1M"
-default['mysql']['tunable']['query_cache_size']     = "16M"
+default['mysql']['tunable']['query_cache_size']     = "0"
 
 default['mysql']['tunable']['log_slow_queries']     = "/var/log/mysql/slow.log"
-default['mysql']['tunable']['long_query_time']      = 2
+default['mysql']['tunable']['long_query_time']      = "2"
 
-default['mysql']['tunable']['innodb_buffer_pool_size']  = "256M"
+default['mysql']['tunable']['innodb_buffer_pool_size']          = "1M"
+default['mysql']['tunable']['innodb_additional_mem_pool_size']  = "1M"
+default['mysql']['tunable']['innodb_log_buffer_size']           = "1M"
+default['mysql']['tunable']['innodb_log_file_size']             = "1M"
+default['mysql']['tunable']['innodb_autoextend_increment']      = "8"
