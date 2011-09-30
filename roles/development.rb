8afdd23400e6802d4f2dd505ff0ca431099904bd
Alistair Stead <alistair.stead@designdisclosure.com>
2011-09-30 12:07:09 +0100

    updated roles

--- roles/development.rb
+++ roles/development.rb
@@ -2,21 +2,21 @@ name "development"
 description "PHP Development system"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 run_list(
-  "ant",
+  # "ant",
   "base::ack",
   "base::mc",
   "base::vim",
   "php",
   "php::pear",
-  "php::codebrowser",
-  "php::codesniffer",
-  "php::copypaste",
-  "php::depend",
-  "php::documentor",
-  "php::phpmd",
-  "php::phpunit",
   "php::vfsStream",
   "php::xdebug",
+  "php::phpunit",
+  # "php::copypaste",
+  # "php::documentor",
+  # "php::codesniffer",
+  # "php::phpmd",
+  # "php::depend",
+  # "php::codebrowser",
   "magento::magetool"
   )
 # Attributes applied if the node doesn't have it set already.
--- roles/nfs-server.rb
+++ roles/nfs-server.rb
@@ -2,7 +2,8 @@ name "nfs-server"
 description "NFS Server for centralised file systems"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 run_list(
-  "nfs::server"
+  "nfs::server",
+  "nfs::exports"
   )
 # Attributes applied if the node doesn't have it set already.
 default_attributes()
